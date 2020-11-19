22c773e43436e4a02bf641879bc3b0163209ec26
Meleron <daniiltrotski487@gmail.com>
2020-11-19 19:46:14 +0300

    Remove obj and bin

---
+++ Blogger/Components/Date.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogger.Components
+{
+    [ViewComponent]
+    public class Date
+    {
+        public string Invoke()
+        {
+            return $"Дата:{DateTime.Now.ToString("dd/mm/yyyy")}";
+        }
+    }
+}
---
+++ Blogger/Components/Timer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogger.Components
+{
+    [ViewComponent]
+    public class Timer
+    {
+        public string Invoke()
+        {
+            return $"Время обновления страницы:{DateTime.Now.ToString("hh:mm:ss")}";
+        }
+    }
+}
